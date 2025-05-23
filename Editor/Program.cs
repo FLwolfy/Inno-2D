d755ba31ed26adae35be2b7339bae6d83005246d
Hsuan-Kai Liao <hl475@duke.edu>
2025-05-23 09:02:20 -0400

    Doc: Upload Basic File Structures

---
+++ Editor/Program.cs
@@ -0,0 +1,12 @@
+﻿using Engine;
+
+namespace Editor;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Class1 c1 = new Class1();
+        Console.WriteLine("Hello, World!");
+    }
+}
